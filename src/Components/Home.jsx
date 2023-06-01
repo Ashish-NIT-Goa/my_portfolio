c62562f17a9270ae187aea2131289b6b35f646c4
Ashish-NIT-Goa <117079261+Ashish-NIT-Goa@users.noreply.github.com>
2023-06-01 11:20:30 +0000

    Make title center aligned

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -28,7 +28,15 @@ const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "15rem", left: "2rem", width: "40rem" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "15rem",
+          left: "2rem",
+          width: "40rem",
+          textAlign: "center",
+        }}
+      >
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
