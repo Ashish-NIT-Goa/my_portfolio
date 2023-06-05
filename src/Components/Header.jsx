22f65235ec59ac3a50812d4ba2a8be2e88f47949
Ashish-NIT-Goa <117079261+Ashish-NIT-Goa@users.noreply.github.com>
2023-06-05 13:55:26 +0000

    modify header nav bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
       <a href="#home">Home</a>
       <a href="#about">About</a>
       <a href="#portfolio">Portfolio</a>
-      <a href="#get-in-touch">Get in Touch</a>
+      {/* <a href="#get-in-touch">Get in Touch</a> */}
       <a href="#footer">Contact</a>
     </div>
   );
